649be2ca2eb58c642c42f6ee3729bbd66afb3cd1
EstebanR054 <esteban.roa7@soy.sena.edu.co>
2024-07-24 22:18:48 -0500

    Ejemplos.

---
+++ Ejercicio0.py
@@ -0,0 +1,97 @@
+#help("keywords")
+
+#Entero=type(9)
+#decimales=type(9.5)
+#print(Entero)
+#print(decimales)
+
+#space_odyssey=(2001)
+#print(space_odyssey)
+
+#var1=(type('Good night & Good luck'))
+#print(type(True))
+
+#result =10 * 3.0
+#result=(type(result))
+#print(result)
+
+#num1=10
+#num2=3
+#result=num1+num2
+#result=num1-num2
+#result=num1*num2
+#result=num1/num2
+#result=num1//num2
+#result=num1%num2
+#result=num1**num2
+#print(result)
+
+
+#total_cars = 100
+#sold_cars = 20
+#total_cars -= sold_cars
+##total_cars
+#print(total_cars)
+
+#num1=10
+#num2=3
+#result=num1/num2
+#print(round(result,2))
+
+#num1=10
+#num2=3
+#result=num1/num2
+#result=int(result)
+#print(str(result))
+
+#msg = 'Primera línea\nSegunda línea\nTercera línea'
+#print(msg)
+
+#msg = 'Valor = \t40'
+#print(msg)
+
+#msg = 'Necesitamos \'escapar\' la comilla simple'
+#print(msg)
+
+#msg = 'Capítulo \\ Sección \\ Encabezado'
+#print(msg)
+
+#msg1 = '¿Sabes por qué estoy acá?'
+#msg2 = 'Porque me apasiona'
+#msg=msg1+' '+msg2
+#print(msg)
+
+#msg1 = '¿Sabes por qué estoy acá?'
+#msg2 = 'Porque me apasiona'
+#print(msg1, msg2)
+
+#msg1 = '¿Sabes por qué estoy acá?'
+#msg2 = 'Porque me apasiona'
+#print(msg1, msg2, sep='|')
+
+#msg1 = '¿Sabes por qué estoy acá?'
+#msg2 = 'Porque me apasiona'
+#print(msg2, end='!!')
+
+#name = input('¿Cómo te llamas?')
+#age = input('¿Cuántos años tienes?')
+#print('Hola, mucho gusto soy '+ name +' y tengo '+ age + ' años .')
+
+#num1=input('Ingresa un número: ')
+#num2=input('Ingresa otro número: ')
+#suma=num1+num2
+#resta=num1-num2
+#multiplicacion=num1*num2
+#division=num1/num2
+#print('La suma de los números es: '+suma+'.')
+
+#sentence='Hola, mundo'
+#print(sentence[0])
+#print(sentence[-1])
+#print(sentence[4])
+# print(sentence[-5])
+
+#name='Elon Musk'
+#age=49
+#fortune=43_300
+#print(f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones')
